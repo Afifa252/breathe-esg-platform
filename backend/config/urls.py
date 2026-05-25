638023a68ef770beae6a80b9ef870a016cf9a6ea
Afifa Khanum <khanafifa758@gmail.com>
2026-05-26 02:38:35 +0530

    Added JWT authentication routes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/', include('dashboard.urls')),
-    path('api/', include('ingestion.urls')),
+    path('api/dashboard/', include('dashboard.urls')),
+    path('api/ingestion/', include('ingestion.urls')),
 
-    path('', TemplateView.as_view(
-        template_name='index.html'
-    )),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
